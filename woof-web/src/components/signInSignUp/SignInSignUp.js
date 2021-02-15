5160306cca5715b58f7d527b625ca0b9aa02207d
JP Reilly <jpreilly@stanford.edu>
2021-02-15 13:00:15 -0800

    now all using hooks

--- woof-web/src/components/signInSignUp/SignInSignUp.js
+++ woof-web/src/components/signInSignUp/SignInSignUp.js
@@ -3,57 +3,34 @@ import "./SignInSignUp.css";
 import SignInBox from "../signInBox/SignInBox.js";
 import SignUpBox from "../signUpBox/SignUpBox.js";
 
-/**
- * This component houses the functionality for displaying sign in
- * and sign up features.
- */
-class SignInSignUp extends React.Component {
-  constructor(props) {
-    super(props);
+function SignInSignUp() {
+  const [actionBox, setActionBox] = useState();
 
-    this.state = {
-      actionBox: undefined,
-    };
-  }
-
-  handleButtonClick(buttonName, event) {
-    console.log("Button name:", buttonName);
-    console.log("Event", event);
-    if (buttonName === "signIn-button") {
-      this.setState({ actionBox: "sign in" });
-    }
-    if (buttonName === "signUp-button") {
-      this.setState({ actionBox: "sign up" });
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="SignInSignUp-Container">
-          <p className="SignInSignUp-WelcomeMsg"> Welcome to Woof </p>
-          <div className="SignInSignUp-Clickables">
-            <button
-              onClick={(e) => this.handleButtonClick("signIn-button", e)}
-              className="SignInSignUp-Button"
-              id="signIn-button"
-            >
-              Sign In
-            </button>
-            <button
-              onClick={(e) => this.handleButtonClick("signUp-button", e)}
-              className="SignInSignUp-Button"
-              id="signUp-button"
-            >
-              Sign Up
-            </button>
-          </div>
+  return (
+    <div>
+      <div className="SignInSignUp-Container">
+        <p className="SignInSignUp-WelcomeMsg"> Welcome to Woof </p>
+        <div className="SignInSignUp-Clickables">
+          <button
+            onClick={() => setActionBox("signIn")}
+            className="SignInSignUp-Button"
+            id="signIn-button"
+          >
+            Sign In
+          </button>
+          <button
+            onClick={() => setActionBox("signUp")}
+            className="SignInSignUp-Button"
+            id="signUp-button"
+          >
+            Sign Up
+          </button>
         </div>
-        {this.state.actionBox === "sign in" ? <SignInBox /> : ""}
-        {this.state.actionBox === "sign up" ? <SignUpBox /> : ""}
       </div>
-    );
-  }
+      {actionBox === "signIn" ? <SignInBox /> : ""}
+      {actionBox === "signUp" ? <SignUpBox /> : ""}
+    </div>
+  );
 }
 
 export default SignInSignUp;
