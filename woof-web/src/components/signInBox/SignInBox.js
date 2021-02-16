6163989c8bc74497a59c71f3c35a6dcc49c3a0a9
Tim Sah <tsah@stanford.edu>
2021-02-16 12:31:43 -0800

    added page redirecting

--- woof-web/src/components/signInBox/SignInBox.js
+++ woof-web/src/components/signInBox/SignInBox.js
@@ -1,10 +1,16 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import AuthContext from "../../contexts/AuthContext";
 import "./SignInBox.css";
 
+/**
+ * Sign in with email/password combo or with Google
+ * [Note, redirect from google sign in still needs work, currently
+ * you have to sign in with Google and then click sign in with Google button
+ * again in order to sign in]
+ */
 function SignInBox() {
   const authApi = useContext(AuthContext);
   const history = useHistory();
@@ -21,52 +27,37 @@ function SignInBox() {
     await firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .then((response) => {
+      .then((result) => {
         // Signed in
-        authApi.setUser(response.user);
-        console.log("Current user: ", response.user);
+        authApi.setUser(result.user);
+        console.log("Current user: ", result.user);
         history.push("/lecture");
       })
       .catch((error) => {
-        let errorCode = error.code;
-        let errorMessage = error.message;
-        console.log("Error with code: ", errorCode);
-        console.log("Error with message: ", errorMessage);
+        console.log("Error with code: ", error.code);
+        console.log("Error with message: ", error.message);
         // Display the error message below in a popup
       });
   }
 
   async function signInGoogleClick() {
     console.log("Attempting Google Sign In");
-    await firebase.auth().signInWithRedirect(provider);
+    firebase
+      .auth()
+      .getRedirectResult()
+      .then((result) => {
+        if (!result.user) {
+          result = firebase.auth().signInWithRedirect(provider);
+        }
+        authApi.setUser(result.user);
+        console.log("Current user: ", result.user);
+        history.push("/lecture");
+      })
+      .catch((error) => {
+        console.log("Error with code: ", error.code);
+        console.log("Error with message: ", error.message);
+      });
   }
-
-  /**
-   * NOTE -- To access Google Provider's OAuth token, use the following
-   * on the newly loaded page
-   * [source: https://firebase.google.com/docs/auth/web/google-signin]
-   */
-  // firebase.auth()
-  // .getRedirectResult()
-  // .then((result) => {
-  //   if (result.credential) {
-  //     /** @type {firebase.auth.OAuthCredential} */
-  //     var credential = result.credential;
-
-  //     // This gives you a Google Access Token. You can use it to access the Google API.
-  //     var token = credential.accessToken;
-  //   }
-  //   // The signed-in user info.
-  //   var user = result.user;
-  // }).catch((error) => {
-  //   // Handle Errors here.
-  //   var errorCode = error.code;
-  //   var errorMessage = error.message;
-  //   // The email of the user's account used.
-  //   var email = error.email;
-  //   // The firebase.auth.AuthCredential type that was used.
-  //   var credential = error.credential;
-  // });
 
   return (
     <div className="SignInBox-Container">
