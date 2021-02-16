9341036cb570df0dd7f5dc10500d3d8c04d652bc
Tim Sah <tsah@stanford.edu>
2021-02-16 10:22:31 -0800

    Google SignIn implemented

--- woof-web/src/components/signInBox/SignInBox.js
+++ woof-web/src/components/signInBox/SignInBox.js
@@ -8,11 +8,12 @@ import "./SignInBox.css";
 function SignInBox() {
   const authApi = useContext(AuthContext);
   const history = useHistory();
+  const provider = new firebase.auth.GoogleAuthProvider();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  async function signInClick() {
+  async function signInEmailClick() {
     console.log("Attempting Sign In with:");
     console.log("Email: ", email);
     console.log("Password: ", password);
@@ -31,14 +32,48 @@ function SignInBox() {
         let errorMessage = error.message;
         console.log("Error with code: ", errorCode);
         console.log("Error with message: ", errorMessage);
+        // Display the error message below in a popup
       });
   }
+
+  async function signInGoogleClick() {
+    console.log("Attempting Google Sign In");
+    await firebase.auth().signInWithRedirect(provider);
+  }
+
+  /**
+   * NOTE -- To access Google Provider's OAuth token, use the following
+   * on the newly loaded page
+   * [source: https://firebase.google.com/docs/auth/web/google-signin]
+   */
+  // firebase.auth()
+  // .getRedirectResult()
+  // .then((result) => {
+  //   if (result.credential) {
+  //     /** @type {firebase.auth.OAuthCredential} */
+  //     var credential = result.credential;
+
+  //     // This gives you a Google Access Token. You can use it to access the Google API.
+  //     var token = credential.accessToken;
+  //   }
+  //   // The signed-in user info.
+  //   var user = result.user;
+  // }).catch((error) => {
+  //   // Handle Errors here.
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  //   // The email of the user's account used.
+  //   var email = error.email;
+  //   // The firebase.auth.AuthCredential type that was used.
+  //   var credential = error.credential;
+  // });
 
   return (
     <div className="SignInBox-Container">
       <p className="SignInBox-WelcomeMsg">
-        Enter your email and password below to SIGN IN:
+        Sign in using one of the two methods below
       </p>
+      <div className="SignInBox-Method"> Sign in with email </div>
       <div className="SignInBox-InputTable">
         <div className="SignInBox-InputRow">
           <label className="SignInBox-InputCell">Email: </label>
@@ -64,11 +99,18 @@ function SignInBox() {
       <div className="SignInBox-InputRow">
         <button
           className="SignInBox-SignUpButton"
-          onClick={() => signInClick()}
+          onClick={() => signInEmailClick()}
         >
           Sign In
         </button>
       </div>
+      <div className="SignInBox-Method"> Sign in with Google </div>
+      <button
+        className="SignInBox-SignUpButton"
+        onClick={() => signInGoogleClick()}
+      >
+        Google Sign In
+      </button>
     </div>
   );
 }
