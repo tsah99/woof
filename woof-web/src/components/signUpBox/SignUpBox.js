7679b1fb6fbe2708c0e9046d7da1718b3d409311
Brian Zeng <bzeng313@stanford.edu>
2021-02-20 16:02:28 -0800

    After sign up, now redirects you to /lectureDashboard instead of /lecture

--- woof-web/src/components/signUpBox/SignUpBox.js
+++ woof-web/src/components/signUpBox/SignUpBox.js
@@ -24,7 +24,7 @@ function SignUpBox() {
         // Signed in
         authApi.setUser(response.user);
         console.log("Current user: ", response.user);
-        history.push("/lecture");
+        history.push("/lectureDashboard");
       })
       .catch((error) => {
         let errorCode = error.code;
